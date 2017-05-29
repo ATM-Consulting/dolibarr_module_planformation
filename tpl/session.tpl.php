f3e0a555e758c57b8cc3670ef5e15c2b4bd2da8f
Marc de Lima Lucio <marc.delimalucio@atm-consulting.fr>
2017-05-29 10:45:22 +0200

    FIX: mauvais fichier supprimé dans l'avant-dernier commit

---
+++ tpl/session.tpl.php
@@ -0,0 +1,56 @@
+<table class="border" width="100%">
+	<tr>
+		<td class="titlefieldcreate">[trans.ref; strconv=no;]</td>
+		<td>[session.ref; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.formation; strconv=no;]</td>
+		<td>[session.formation; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.statut; strconv=no;]</td>
+		<td>[session.statut; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.interne_externe; strconv=no;]</td>
+		<td>[session.interne_externe; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.date_debut; strconv=no;]</td>
+		<td>[session.date_debut; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.date_fin; strconv=no;]</td>
+		<td>[session.date_fin; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.duree_planifiee; strconv=no;]</td>
+		<td>[session.duree_planifiee; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.opca; strconv=no;]</td>
+		<td>[session.opca; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.budget; strconv=no;]</td>
+		<td>[session.budget; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.budget_consomme; strconv=no;]</td>
+		<td>[session.budget_consomme; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.prise_en_charge_estimee; strconv=no;]</td>
+		<td>[session.prise_en_charge_estimee; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.prise_en_charge_acceptee; strconv=no;]</td>
+		<td>[session.prise_en_charge_acceptee; strconv=no;]</td>
+	</tr>
+	<tr>
+		<td class="titlefieldcreate">[trans.prise_en_charge_reelle; strconv=no;]</td>
+		<td>[session.prise_en_charge_reelle; strconv=no;]</td>
+	</tr>
+</table>
+
+<div class="tabsAction">[buttons; strconv=no]</div>
